eb98b2879b67f45fcf5c6f1e5e4f9f07a363e204
unknown <softech.series@gmail.com>
2023-11-22 22:52:27 +0700

    first commit

---
+++ resources/views/pelanggan/component/footer.blade.php
@@ -0,0 +1,35 @@
+<div class="d-flex justify-content-between gap-5 pt-4 mt-5">
+    <div class="title-left w-25">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            TOKO BUNGA MAS DICKA
+        </div>
+        <p>Menyediakan berbagai macam bunga dari yang hidup di air hingga hias,dari yang murah dan mahal,dan
+            dari berbagai belahan dunia.</p>
+    </div>
+    <div class="title-middle w-25">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            Tentang Kami
+        </div>
+        <ul class="list-unstyled">
+            <li>
+                <p>
+                    Mahasiswa jurusan TEKNK INFORMATIKA prodi TEKNIK REKAYASA PERANGKAT LUNAK kelas 2D
+                </p>
+            </li>
+        </ul>
+    </div>
+    <div class="title-sosmed w-30">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            Sosial Media
+        </div>
+        <div class="sosmed d-flex gap-3">
+            <i class="fa-brands fa-instagram fa-2x"></i>
+            <i class="fa-brands fa-square-facebook fa-2x"></i>
+            <i class="fa-brands fa-whatsapp fa-2x"></i>
+            <i class="fa-brands fa-linkedin fa-2x"></i>
+        </div>
+    </div>
+</div>
+<div class="text-center p-lg-4 mt-3">
+    Copy right @ mas DICKA 2023</i>
+</div>
